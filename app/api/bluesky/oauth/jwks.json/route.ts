8888719d510ac47278148dac92dc4b9ec1600f9e
Eric Liu <14352200+eliu-dev@users.noreply.github.com>
2025-05-08 17:51:29 -0400

    jwks prod fix attempt

--- app/api/bluesky/oauth/jwks.json/route.ts
+++ app/api/bluesky/oauth/jwks.json/route.ts
@@ -3,14 +3,20 @@ import fs from 'fs';
 import path from 'path';
 
 export async function GET() {
-  const publicJwksPath = path.join(
-    process.cwd(),
-    './app/api/bluesky/oauth/jwks.json/jwks.json'
-  );
-  const publicJwks = JSON.parse(fs.readFileSync(publicJwksPath, 'utf8'));
-  console.log(publicJwks);
+  if (process.env.NODE_ENV === 'development') {
+    const publicJwksPath = path.join(
+      process.cwd(),
+      './app/api/bluesky/oauth/jwks.json/jwks.json'
+    );
+    const publicJwks = JSON.parse(fs.readFileSync(publicJwksPath, 'utf8'));
+    console.log(publicJwks);
 
-  return NextResponse.json(publicJwks, {
-    status: 200,
-  });
+    return NextResponse.json(publicJwks, {
+      status: 200,
+    });
+  } else {
+    const res = await fetch('https://static.blueskyscope.com/jwks.json');
+    const publicJwks = await res.json();
+    return NextResponse.json(publicJwks, { status: 200 });
+  }
 }

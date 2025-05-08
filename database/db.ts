9d641e4beb470648b2090e3e26a15ae4e6005f98
Eric Liu <14352200+eliu-dev@users.noreply.github.com>
2025-05-08 16:47:32 -0400

    fix prod database logic in db.ts

--- database/db.ts
+++ database/db.ts
@@ -1,7 +1,12 @@
 import { neonConfig, Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { NeonDatabase, drizzle as neonDrizzle } from 'drizzle-orm/neon-serverless';
+
+import { NodePgDatabase, drizzle as pgDrizzle } from 'drizzle-orm/node-postgres';
+
 import * as schema from './schema';
 import { WebSocket } from 'ws';
+
+let db: NeonDatabase<typeof schema> | NodePgDatabase<typeof schema>;
 
 const connection =
   process.env.NODE_ENV === 'production'
@@ -11,13 +16,19 @@ const connection =
 const pool = new Pool({ connectionString: connection });
 
 if (process.env.NODE_ENV === 'production') {
-  neonConfig.webSocketConstructor = WebSocket;
-  neonConfig.poolQueryViaFetch = true;
+  db = pgDrizzle({
+    connection: {
+      connectionString: connection,
+      ssl: true,
+    },
+    schema
+  });
 } else {
   neonConfig.wsProxy = (host) => `${host}:5433/v1`;
   neonConfig.useSecureWebSocket = false;
   neonConfig.pipelineTLS = false;
   neonConfig.pipelineConnect = false;
+  db = neonDrizzle(pool, { schema });
 }
 
-export const db = drizzle(pool, { schema });
+export { db };
